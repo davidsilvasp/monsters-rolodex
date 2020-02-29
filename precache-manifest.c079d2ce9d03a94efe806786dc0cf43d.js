4575716a3204a3fc5968a5ba2c228a010526cd15
David Barbosa <davidsilva.sp@outlook.com>
2020-02-29 00:06:45 -0300

    Updates

---
+++ precache-manifest.c079d2ce9d03a94efe806786dc0cf43d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3fd3c5089c76eb12504c6acb470e357e",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "cea54a9259379202cac5",
+    "url": "/monsters-rolodex/static/css/main.fa44559b.chunk.css"
+  },
+  {
+    "revision": "2d6602caf790a4fd2697",
+    "url": "/monsters-rolodex/static/js/2.1f3314c8.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/monsters-rolodex/static/js/2.1f3314c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cea54a9259379202cac5",
+    "url": "/monsters-rolodex/static/js/main.0ef73424.chunk.js"
+  },
+  {
+    "revision": "9e014af7b12dc10ef867",
+    "url": "/monsters-rolodex/static/js/runtime-main.ff038b7c.js"
+  }
+]);
